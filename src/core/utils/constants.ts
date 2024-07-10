b46b566cad1f0fa0d61302b16b42a520d7383bf7
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2024-07-10 15:17:05 +0545

    fix: constant types

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -1,4 +1,4 @@
 export const constants = {
-    baseUrl: process.env.NEXT_PUBLIC_SERVER_URL,
-    reCaptchaKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+    baseUrl: process.env.NEXT_PUBLIC_SERVER_URL ?? "",
+    reCaptchaKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "",
 };
