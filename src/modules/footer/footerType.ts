d5d5db4b861c37fdc6ece3fa813d14bf1e421472
Avishek Dahal <dahalavishek2022@gmail.com>
2025-02-18 11:31:00 +0545

    refactor: footer api and integration

---
+++ src/modules/footer/footerType.ts
@@ -0,0 +1,13 @@
+export type Footer = {
+    id: number,
+    copyright: string,
+    hotline: string,
+    address: string,
+    email: string,
+    aboutUs: string,
+}
+
+export type FooterResultType = {
+    status: string,
+    data: Array<Footer>
+}
