f24cee83d26f3feb30209b92b6310a2f07c26267
joshibineet <joshibineet123@gmail.com>
2025-03-07 15:20:48 +0545

    fix: issue fixed default text

--- src/app/(home)/news/page.tsx
+++ src/app/(home)/news/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = customMetaDataGenerator({
   ogImage: 'https://altitudeairnepal.com/images/banner/banner-2.webp',
 });
 export default async function NewsList() {
-  const { data: newsData, error: bodMessageDataError } = await fetchData<
+  const { data: newsData, error: newsMessageDataError } = await fetchData<
     ObjectResponseType<NewsDataType[]>
   >(apiPaths.getNewsUrl);
 
@@ -26,10 +26,14 @@ export default async function NewsList() {
         <div className="container mx-auto">
           <h2 className="text-center text-xl font-bold mb-10">IN THE NEWS</h2>
           <div className="grid px-3 sm:px-0 grid-cols-12 gap-y-10 gap-x-0 sm:gap-x-10 max-w-6xl mx-auto my-5">
-            {!bodMessageDataError &&
-              newsData?.data.map((item: any) => {
-                return <NewsItem key={item.id} data={item} />;
-              })}
+            {!newsMessageDataError &&
+              (newsData?.data && newsData.data.length > 0 ? (
+                newsData.data.map((item: any) => (
+                  <NewsItem key={item.id} data={item} />
+                ))
+              ) : (
+                <div className="news-empty">No news items available.</div>
+              ))}
           </div>
         </div>
       </section>
