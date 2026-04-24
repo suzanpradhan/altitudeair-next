791274f5ecfdbc0ec798f2fdd87b9a5f75451b6a
bishal khatri <bishal.khatri343@outlook.com>
2026-04-24 11:54:19 +0545

    url added on config

--- next.config.js
+++ next.config.js
@@ -42,6 +42,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'altitudeair.iionstech.com.np',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
